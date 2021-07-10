6832130e46f89a550db590796aa45dac609ce898
googol88 <ronaldfleung@gmail.com>
2021-07-10 19:11:42 -0400

    ✨ Create ideas

--- main.py
+++ main.py
@@ -11,13 +11,58 @@ def create_connection(db_file):
     conn = None;
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    return conn
+
+def create_ideas_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print("SQL error: ") 
+        print(e)
+
+def create_idea(conn, idea):
+    """
+    Add a new idea into the ideas table
+    :param conn:
+    :param idea:
+    :return: idea id
+    """
+    sql = """INSERT INTO ideas(name,description)
+             VALUES(?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, idea)
+    conn.commit()
+    return cur.lastrowid
+
+def main():
+    database = os.environ.get("DB_FILE_PATH")
+
+    sql_create_ideas_table =  """CREATE TABLE IF NOT EXISTS ideas (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    description text
+                                );"""
+     
+    # create a database connection
+    conn = create_connection(database) # if os.path.exists(database) else create_connection(r"C:\sqlite\db\pythonsqlite.db")
+
+    # create tables
+    if conn is not None:
+        # create ideas table
+        create_ideas_table(conn, sql_create_ideas_table)
+        create_idea(conn, ("Test idea", "This is a test idea"))
+        create_idea(conn, ("Test idea 2", "This is another test idea"))
+    else:
+        print("Error! cannot create the database connection.")
 
 if __name__ == '__main__':
-    print(os.environ.get("DB_FILE"))
-    create_connection(os.environ.get("DB_FILE")) # r"C:\sqlite\db\pythonsqlite.db"
+    main()
